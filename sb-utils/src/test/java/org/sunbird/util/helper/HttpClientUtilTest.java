0a974192e1291824d629462fabc80b983f0e046e
sknirmalkar89 <satishkumar.nirmalkar@tarento.com>
2021-12-02 09:58:49 +0530

    SB-27369: junit test add for group service

--- sb-utils/src/test/java/org/sunbird/util/helper/HttpClientUtilTest.java
+++ sb-utils/src/test/java/org/sunbird/util/helper/HttpClientUtilTest.java
@@ -182,7 +182,6 @@ public class HttpClientUtilTest {
         Mockito.when(httpClientBuilder.build()).thenReturn(httpClient);
         Mockito.when(HttpClients.custom()).thenReturn(httpClientBuilder);
         HttpClientUtil.getInstance();
-        
         Map<String,Object> reqContext = new HashMap<>();
         reqContext.put("userid","123123123");
         Map<String,String> headers = new HashMap<>();
