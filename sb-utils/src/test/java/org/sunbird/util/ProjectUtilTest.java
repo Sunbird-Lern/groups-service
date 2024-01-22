fd80a747568304582caa2107ffa65f89e9731983
Harikumar Palemkota <harip@ilimi.in>
2024-01-22 12:55:56 +0530

    Removedd unnecessary property files for better maintaince-1

--- sb-utils/src/test/java/org/sunbird/util/ProjectUtilTest.java
+++ sb-utils/src/test/java/org/sunbird/util/ProjectUtilTest.java
@@ -3,19 +3,20 @@ package org.sunbird.util;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import org.sunbird.cache.util.Platform;
 import org.sunbird.common.util.JsonKey;
 
 public class ProjectUtilTest {
 
   @Test
   public void testGetConfigValueWithExistsInPropertyFile() {
-    String exists = ProjectUtil.getConfigValue(JsonKey.SUNBIRD_HEALTH_CHECK_ENABLE);
+    String exists = Platform.getBoolean(JsonKey.SUNBIRD_HEALTH_CHECK_ENABLE, false).toString();
     assertEquals("true", exists);
   }
 
   @Test
   public void testGetConfigValueWithNotExistsInPropertyFile() {
-    String exists = ProjectUtil.getConfigValue("sunbird_health_check_not_enable");
-    assertNull(exists);
+    Boolean exists = Platform.getBoolean("sunbird_health_check_not_enable", false);
+    assertFalse(exists);
   }
 }
